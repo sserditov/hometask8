d8d494ea4d562ac10bdc54a88881f488e67ec15a
sserditov <108936913+sserditov@users.noreply.github.com>
2022-08-09 15:44:40 +0500

    hometask8

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int counter = N;
+while (counter <= N)
+{
+    Console.Write(counter + " ");
+    counter++;
+}
